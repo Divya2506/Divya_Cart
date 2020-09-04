8f24feb24783bf982d11239966293efae49a06d4
divya <dpj2506@gmail.com>
2020-09-04 13:22:14 +1200

    version-1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All Rights Reserved @ Divya's Cart
+      </footer>
+    </div>
+  );
+}
+
+export default App;
